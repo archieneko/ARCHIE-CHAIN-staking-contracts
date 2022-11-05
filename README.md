b9a07c34c148c37685d6f5f3048415d0b6e860e1
blockinator <36169687+blockinator@users.noreply.github.com>
2022-11-04 18:03:17 -0700

    Initial Upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# staking-contracts
+
+Smart contracts for Archie Chain
+
+## How to start
+
+```shell
+$ git clone https://github.com/archieneko/staking-contracts.git
+$ cd staking-contracts
+$ npm i
+```
+
+### Build contracts
+
+```shell
+$ npm run build
+```
+
+### Run unit tests
+
+```shell
+$ npm run test
+```
+
+### Deploy contract to ArchieChain
+
+```shell
+$ npm run deploy
+```
+
+### Stake balance to contract
+
+Please make sure required values are set in .env to use this command
+
+```shell
+$ npm run stake
+```
+
+### Unstake from contract
+
+```shell
+$ npm run unstake
+```
+
+### Check current total staked amount and validators in contract
+
+```shell
+$ npm run info
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+import {HardhatUserConfig} from "hardhat/types";
+import "@nomiclabs/hardhat-waffle"
+import "tsconfig-paths/register";
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+
+require("dotenv").config();
+
+const privateKeys = (process.env.PRIVATE_KEYS ?? "0000000000000000000000000000000000000000000000000000000000000000").split(",")
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.7",
+  networks: {
+    mainnet: {
+      url: process.env.JSONRPC_URL ?? "https://rpc-main-1.archiechain.io",
+      accounts: [
+          ...privateKeys,
+      ],
+    },
+    testnet: {
+      url: process.env.JSONRPC_URL ?? "https://rpc-test-1.archiechain.io",
+      accounts: [
+          ...privateKeys,
+      ],
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 21
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false,
+    externalArtifacts: ["externalArtifacts/*.json"],
+  },
+};
+
+export default config;
